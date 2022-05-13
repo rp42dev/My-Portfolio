ac6d7d6bae4e4f33a4f12761eabfc8cfd8899b51
Raivis80 <77964146+Raivis80@users.noreply.github.com>
2022-05-13 22:51:36 +0100

    test service worker

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -7,6 +7,12 @@ import AboutApp from './about/About';
 import ProjectsApp from './projects/Projects';
 import ContactApp from './contact/Contact';
 
+if ('serviceWorker' in navigator) {
+
+  navigator.serviceWorker.register('sw.js')
+    .then(reg => console.log('service worker registered', reg))
+    .catch(err => console.log('service worker not registered', err));
+}
 
 
 function App() {

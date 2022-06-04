57cbc542f5df646562d7a10a49456fc26d02a750
Raivis80 <77964146+Raivis80@users.noreply.github.com>
2022-06-04 11:37:32 +0100

    added styling

--- my-portfolio/src/components/nav/PcNav.js
+++ my-portfolio/src/components/nav/PcNav.js
@@ -1,31 +1,29 @@
-import * as React from 'react';
-import Tab from '@mui/material/Tab';
-import Tabs from '@mui/material/Tabs';
-import { useContext } from 'react';
-import { AppContext } from '../../AppContext.js';
-import { useTheme } from '@mui/material/styles';
-
+import * as React from "react";
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
+import { useContext } from "react";
+import { AppContext } from "../../AppContext.js";
 
 export default function PcMenu(props) {
-    const context = useContext(AppContext);
-    let theme = useTheme();
+  const context = useContext(AppContext);
 
-    return (
-      <React.Fragment>
-        <Tabs
-          value={context.store}
-          onChange={props.handleChange}
-          color={
-            context.colorMode === "dark"
-              ? theme.palette.primary.light
-              : theme.palette.primary.main
-          }
-          indicatorColor="secondary"
-        >
-          {props.myObject.map((option, index) => (
-            <Tab key={index} label={option} value={option}></Tab>
-          ))}
-        </Tabs>
-      </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <Tabs
+        value={context.store}
+        onChange={props.handleChange}
+        indicatorColor="secondary"
+        textColor="secondary"
+      >
+        {props.myObject.map((option, index) => (
+          <Tab
+            sx={{ color: "#D8D8D8" }}
+            key={index}
+            label={option}
+            value={option}
+          ></Tab>
+        ))}
+      </Tabs>
+    </React.Fragment>
+  );
 }

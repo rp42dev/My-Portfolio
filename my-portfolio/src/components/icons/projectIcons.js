493913bda84089143c4833805c90e8ecd33d5674
Raivis80 <77964146+Raivis80@users.noreply.github.com>
2022-07-05 12:03:10 +0100

    aadded project

--- my-portfolio/src/components/icons/projectIcons.js
+++ my-portfolio/src/components/icons/projectIcons.js
@@ -5,19 +5,50 @@ import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 
 
 export default function ProjectIcons(props) {
+    
+    if (props.github === undefined){
+        console.log(props);
+    }
     return (
-        <Stack direction="row" spacing={2}>
+      <Stack direction="row" spacing={2}>
+        {props.github !== undefined ? (
+          <>
             <Tooltip title="On GitHub">
-                <IconButton size="small" component={Link} onClick={() => window.open(props.github, '_blank')} color='secondary' aria-label="On GitHub">
-                    <GitHubIcon />
-
-                </IconButton>
+              <IconButton
+                size="small"
+                component={Link}
+                onClick={() => window.open(props.github, "_blank")}
+                color="secondary"
+                aria-label="On GitHub"
+              >
+                <GitHubIcon />
+              </IconButton>
             </Tooltip>
             <Tooltip title="Website">
-                <IconButton size="small" component={Link} onClick={() => window.open(props.url, '_blank')} color='secondary' aria-label="Website">
-                    <RemoveRedEyeIcon sx={{ fontSize: 30 }} />
-                </IconButton>
+              <IconButton
+                size="small"
+                component={Link}
+                onClick={() => window.open(props.url, "_blank")}
+                color="secondary"
+                aria-label="Website"
+              >
+                <RemoveRedEyeIcon sx={{ fontSize: 30 }} />
+              </IconButton>
             </Tooltip>
-        </Stack>
+          </>
+        ) : (
+          <Tooltip title="Website">
+            <IconButton
+              size="small"
+              component={Link}
+              onClick={() => window.open(props.url, "_blank")}
+              color="secondary"
+              aria-label="Website"
+            >
+              <RemoveRedEyeIcon sx={{ fontSize: 30 }} />
+            </IconButton>
+          </Tooltip>
+        )}
+      </Stack>
     );
 }
--- my-portfolio/src/projects/data/ProjectData.js
+++ my-portfolio/src/projects/data/ProjectData.js
@@ -1,5 +1,6 @@
 
-import image1 from '../../assets/images/tattoo.png';
+import image from '../../assets/images/fltd.png';
+import image1 from "../../assets/images/tattoo.png";
 import image2 from '../../assets/images/hats.png';
 import image3 from '../../assets/images/emotify.png';
 import image4 from '../../assets/images/game.png';
@@ -7,46 +8,59 @@ import image5 from '../../assets/images/flower.png';
 
 
 const itemData = [
-    {
-        img: image1,
-        title: 'Tattoo Sk Workshop',
-        description: 'Django Full Stack project created for a Tattoo artist business in Norway. Features Two Languages and backend functionality allowing owner to easily store and manipulate an image gallery and other content.',
-        url: 'https://tattooskworkshop.no/',
-        github: 'https://github.com/Raivis80/TattooSkWorkshop',
-        tech: ['HTML', 'CSS', 'JS', 'Django', 'PostgreSQL', 'Engin-x']
-    },
-    {
-        img: image2,
-        title: 'A Hat Shop',
-        description: 'Django full-stack site based around business logic. With an user authentication, Stripe payment service, relational database schema to store and manipulate data records and structured and navigation.',
-        url: 'https://a-hat-shop.herokuapp.com/',
-        github: 'https://github.com/Raivis80/MS-4-Django-Full-Stack-Project',
-        tech: ['HTML', 'CSS', 'JS', 'Django', 'PostgreSQL', 'AWS']
-    },
-    {
-        img: image3,
-        title: 'Emotify',
-        description: "Flask full stack project with user authentication allowing users register and log in. Authenticated users are able to store and manipulate data records. Data handling: build on MongoDB-backed CRUD functionality.",
-        url: 'https://emotify-ms-3.herokuapp.com/',
-        github: 'https://github.com/Raivis80/Milestone-Project-3',
-        tech: ['HTML', 'CSS', 'JS', 'Flask', 'MongoDB', 'Cloudinary']
-    },
-    {
-        img: image4,
-        title: 'The Game',
-        description: "Simple JavaScript, single-player game, the objective of this game is to click or tap the objects spawning randomly as quickly as you can. The difficulty will increase throughout the game",
-        url: 'https://raivis80.github.io/The-Game/',
-        github: 'https://github.com/Raivis80/The-Game',
-        tech: ['HTML', 'CSS', 'JS', 'EmailJs']
-    },
-    {
-        img: image5,
-        title: 'Floral Fantasy',
-        description: 'This website was created using primarily HTML5 and CSS3. The main focus for the site is to create an online presence allowing users to view various flower arrangements from the best florists.',
-        url: 'https://raivis80.github.io/First-Milestone-Project/',
-        github: 'https://github.com/Raivis80/First-Milestone-Project',
-        tech: ['HTML', 'CSS', 'JS', 'Bootstrap']
-    },
+  {
+    img: image,
+    title: "From Lads To Dads",
+    description:
+      "Volunteering as a web designer and  developer on the project From lads to dads. This program will provide practical and emotional support to equip fathers with the necessary tools, guidance for becoming a Dad",
+    url: "https://www.fromladstodads.com/",
+    tech: ["HTML", "CSS", "JS", "Squarespace"],
+  },
+  {
+    img: image1,
+    title: "Tattoo Sk Workshop",
+    description:
+      "Django Full Stack project created for a Tattoo artist business in Norway. Features Two Languages and backend functionality allowing owner to easily store and manipulate an image gallery and other content.",
+    url: "https://tattooskworkshop.no/",
+    github: "https://github.com/Raivis80/TattooSkWorkshop",
+    tech: ["HTML", "CSS", "JS", "Django", "PostgreSQL", "Engin-x"],
+  },
+  {
+    img: image2,
+    title: "A Hat Shop",
+    description:
+      "Bootcamp Milestone Project 4. This is a Django full-stack site based around business logic. With an user authentication, Stripe payment service, relational database schema to store and manipulate data records and structured and navigation.",
+    url: "https://a-hat-shop.herokuapp.com/",
+    github: "https://github.com/Raivis80/MS-4-Django-Full-Stack-Project",
+    tech: ["HTML", "CSS", "JS", "Django", "PostgreSQL", "AWS"],
+  },
+  {
+    img: image3,
+    title: "Emotify",
+    description:
+      "Bootcamp Milestone Project 3. A Flask full stack project with user authentication allowing users register and log in. Authenticated users are able to store and manipulate data records. Data handling: build on MongoDB-backed CRUD functionality.",
+    url: "https://emotify-ms-3.herokuapp.com/",
+    github: "https://github.com/Raivis80/Milestone-Project-3",
+    tech: ["HTML", "CSS", "JS", "Flask", "MongoDB", "Cloudinary"],
+  },
+  {
+    img: image4,
+    title: "The Game",
+    description:
+      "Bootcamp Milestone Project 2. Simple JavaScript, single-player game, the objective of this game is to click or tap the objects spawning randomly as quickly as you can. The difficulty will increase throughout the game",
+    url: "https://raivis80.github.io/The-Game/",
+    github: "https://github.com/Raivis80/The-Game",
+    tech: ["HTML", "CSS", "JS", "EmailJs"],
+  },
+  {
+    img: image5,
+    title: "Floral Fantasy",
+    description:
+      "Bootcamp Milestone Project 1. This website was created using primarily HTML5 and CSS3. The main focus for the site is to create an online presence allowing users to view various flower arrangements from the best florists.",
+    url: "https://raivis80.github.io/First-Milestone-Project/",
+    github: "https://github.com/Raivis80/First-Milestone-Project",
+    tech: ["HTML", "CSS", "JS", "Bootstrap"],
+  },
 ];
 export default itemData;
 

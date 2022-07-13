51f101e21e37c28982041720e94268ef4f776508
Raivis80 <77964146+Raivis80@users.noreply.github.com>
2022-07-13 16:09:58 +0100

    added link

--- my-portfolio/src/projects/Projects.js
+++ my-portfolio/src/projects/Projects.js
@@ -1,9 +1,11 @@
 import {
   Typography,
-  Box, Container,
+  Box,
+  Container,
   IconButton,
   Tooltip,
-} from '@mui/material';
+  Link,
+} from "@mui/material";
 import ProjectsComponent from '../components/projects/projectsComponent.js';
 import itemData from './data/ProjectData';
 import { AppContext } from '../AppContext.js';
@@ -22,14 +24,19 @@ function ProjectsApp() {
   }
 
   return (
-
     <Container id="projects">
-
-      <InView threshold={.14} as="div" onChange={(inView, entry) => { if (inView === true) handlePage() }}>
-        <Box sx={{ position: 'relative', height: '100%', overflow: 'hidden' }}>
-
+      <InView
+        threshold={0.14}
+        as="div"
+        onChange={(inView, entry) => {
+          if (inView === true) handlePage();
+        }}
+      >
+        <Box sx={{ position: "relative", height: "100%", overflow: "hidden" }}>
           <Box sx={{ mt: 2 }}>
-            <Typography color="secondary" variant="h2">Projects</Typography>
+            <Typography color="secondary" variant="h2">
+              Projects
+            </Typography>
             <Typography sx={{ mt: 2, mb: 3 }} color="primary" variant="h6">
               A collection of some of my work
             </Typography>
@@ -41,8 +48,15 @@ function ProjectsApp() {
             <Typography color="primary.dark" variant="body1">
               More projects on my GitHub page
               <Tooltip title="On GitHub">
-                <IconButton color="secondary" aria-label="delete">
-                  <GitHubIcon fontSize='large' />
+                <IconButton
+                  component={Link}
+                  onClick={() =>
+                    window.open("https://github.com/Raivis80", "_blank")
+                  }
+                  color="secondary"
+                  aria-label="delete"
+                >
+                  <GitHubIcon fontSize="large" />
                 </IconButton>
               </Tooltip>
             </Typography>
